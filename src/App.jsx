c0d8bfc82770bca43f53b7c513f0d1d604208a40
zouk5574-stack <zouk5574@gmail.com>
2025-09-28 04:10:08 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,29 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import MainLayout from "./layouts/MainLayout";
-import Home from "./pages/Home";
-import Produits from "./pages/Produits";
-import Commandes from "./pages/Commandes";
-import Messages from "./pages/Messages";
-import Paiements from "./pages/Paiements";
-import Parametres from "./pages/Parametres";
-import Auth from "./pages/Auth";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout.jsx";
+import Home from "./pages/Home.jsx";
+import Produits from "./pages/Produits.jsx";
+import Commandes from "./pages/Commandes.jsx";
+import Messages from "./pages/Messages.jsx";
+import Paiements from "./pages/Paiements.jsx";
+import Parametres from "./pages/Parametres.jsx";
+import Auth from "./pages/Auth.jsx";
 
-export default function App() {
+function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Home />} />
-          <Route path="produits" element={<Produits />} />
-          <Route path="commandes" element={<Commandes />} />
-          <Route path="messages" element={<Messages />} />
-          <Route path="paiements" element={<Paiements />} />
-          <Route path="parametres" element={<Parametres />} />
-          <Route path="auth" element={<Auth />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/produits" element={<Produits />} />
+          <Route path="/commandes" element={<Commandes />} />
+          <Route path="/messages" element={<Messages />} />
+          <Route path="/paiements" element={<Paiements />} />
+          <Route path="/parametres" element={<Parametres />} />
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
+
+export default App;
