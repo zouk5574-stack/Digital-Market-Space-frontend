ddedf5cfb1e96c10f668b415cf8d1d4701615c46
zouk5574-stack <zouk5574@gmail.com>
2025-09-28 06:13:23 +0100

    Create links.jsx

---
+++ src/pages/seller/links.jsx
@@ -0,0 +1,7 @@
+import { FaBox, FaChartLine, FaWallet } from "react-icons/fa";
+
+export const sellerLinks = [
+  { to: "/seller/dashboard", label: "Mes produits", icon: <FaBox /> },
+  { to: "/seller/sales", label: "Ventes", icon: <FaChartLine /> },
+  { to: "/seller/wallet", label: "Portefeuille", icon: <FaWallet /> },
+];
