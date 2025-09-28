acd256d636a247ceb4bd42006066066fdf7f421f
zouk5574-stack <zouk5574@gmail.com>
2025-09-28 05:57:35 +0100

    Create Footer.jsx

---
+++ shared/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="bg-primary text-white text-center p-4 mt-6">
+      © {new Date().getFullYear()} Digital Market Space – Votre satisfaction est notre priorité
+    </footer>
+  );
+}
+
+export default Footer;
