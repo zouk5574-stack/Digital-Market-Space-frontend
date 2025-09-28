07416daa81a3083554f094f28c81d0c3976adcb5
zouk5574-stack <zouk5574@gmail.com>
2025-09-28 03:43:11 +0100

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'logo.png'],
+      manifest: {
+        name: 'Digital Market Space',
+        short_name: 'DMS',
+        description: 'Votre satisfaction, est notre priorité 🚀',
+        theme_color: '#009688',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/logo.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
+});
