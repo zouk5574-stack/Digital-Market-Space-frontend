5c60f8cc6da8cd0f6d172365c5da4421e8e23db3
zouk5574-stack <zouk5574@gmail.com>
2025-09-28 04:31:26 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,34 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'logo.png'],
+      registerType: "autoUpdate",
+      includeAssets: ["logo.png", "icon-transparent.png"],
       manifest: {
-        name: 'Digital Market Space',
-        short_name: 'DMS',
-        description: 'Votre satisfaction, est notre priorité 🚀',
-        theme_color: '#009688',
-        background_color: '#ffffff',
-        display: 'standalone',
-        start_url: '/',
+        name: "Digital Market Space",
+        short_name: "DMS",
+        start_url: ".",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#2563eb",
         icons: [
           {
-            src: '/logo.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
+            src: "/logo.png",
+            sizes: "512x512",
+            type: "image/png"
+          },
+          {
+            src: "/icon-transparent.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable"
           }
         ]
       }
     })
-  ],
+  ]
 });
