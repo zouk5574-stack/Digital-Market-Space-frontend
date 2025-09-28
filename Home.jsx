d70bd9c28773c799a888bc26cfcad11b67be9095
zouk5574-stack <zouk5574@gmail.com>
2025-09-28 03:55:11 +0100

    Create Home.jsx

---
+++ Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="text-center">
+      <img src="/logo.png" alt="DMS Logo" className="w-40 mx-auto mb-4" />
+      <h1 className="text-4xl font-bold text-teal-600">Bienvenue sur Digital Market Space</h1>
+      <p className="mt-4 text-lg text-gray-600 italic">Votre satisfaction, est notre priorité 🚀</p>
+    </div>
+  );
+}
