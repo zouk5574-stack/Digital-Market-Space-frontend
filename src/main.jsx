00bae38458425196f7e0fe79ee13a257c0a147a2
zouk5574-stack <zouk5574@gmail.com>
2025-09-28 03:44:19 +0100

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { registerSW } from 'virtual:pwa-register';
+
+registerSW(); // ✅ Active la PWA
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
