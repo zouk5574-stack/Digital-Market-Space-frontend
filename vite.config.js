48c007754986182d016266153a6b1506bc0cf762
zouk5574-stack <zouk5574@gmail.com>
2025-09-29 14:20:31 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,16 @@ export default defineConfig({
   server: {
     port: 5173
   }
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: "dist",
+  },
 });
