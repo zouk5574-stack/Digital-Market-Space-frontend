80adb00eab8d0c933bbd1a0448986902eb9c3bb4
zouk5574-stack <zouk5574@gmail.com>
2025-09-29 17:05:15 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 // vite.config.js
 import { defineConfig } from "vite";
-  import{"@vitejs/plugin-react": "^4.3.1"}
-  "vite": "^5.0.0"
-}
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,   // ou 3000 si tu préfères, mais 5173 est standard avec Vite
-    open: true    // ouvre automatiquement ton navigateur en local
+    port: 5173,   // tu peux mettre 3000 si tu préfères
+    open: true    // ouvre automatiquement le navigateur en local
   },
   build: {
     outDir: "dist"
